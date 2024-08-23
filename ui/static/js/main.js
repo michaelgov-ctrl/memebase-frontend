ca76c9f3c2b899706d8040494aca4115d4c571bf
Michael Governanti <mgovernanti@webstaurantstore.com>
2024-08-23 16:34:37 -0400

    js addition for footer day of week

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,3 +1,6 @@
+var dayOfWeek = new Date().toLocaleString('en-us', {  weekday: 'long' })
+document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].innerHTML = "Have a nice " + dayOfWeek; //change content
+
 var navLinks = document.querySelectorAll("nav a");
 for (var i = 0; i < navLinks.length; i++) {
 	var link = navLinks[i]
